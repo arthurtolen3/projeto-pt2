5bda4c3dd950fbc8ddcc9c1620a2181e65b7662f
Arthur Tolentino <97926590+arthurtolen3@users.noreply.github.com>
2022-02-17 18:23:51 -0300

    Delete PROJETO F directory

--- PROJETO F/app/app.js
+++
@@ -1,8 +0,0 @@
-console.log('teste');
-
-document.getElementById('button').addEventListener(`click`, function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
-});
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = 'none'
-});
